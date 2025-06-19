d32d332154634eabcf5ac69817c4f015718f545d
xGodai <Thomas_Brown_@Outlook.com>
2025-06-19 11:27:42 +0100

    Add Dropdown Functionality for Navbar

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,3 +26,24 @@ function sendMessage() {
     input.value = "";
   }
 }
+
+// Dropdown functionality for the navbar
+document
+      .querySelectorAll(".navbar-collapse .nav-link")
+      .forEach((link) => {
+        link.addEventListener("click", function (e) {
+          let section = document.querySelector(e.target.getAttribute("href"));
+          if (section) {
+            e.preventDefault(); // Prevent default anchor click behavior
+            let navbarHeight =
+              document.querySelector(".navbar-toggler").offsetHeight;
+            window.scroll({
+              top: section.offsetTop - navbarHeight, // Adjust for navbar height
+              behavior: "smooth",
+            });
+            document
+              .querySelector(".navbar-collapse")
+              .classList.remove("show"); // Collapse navbar
+          }
+        });
+      });
